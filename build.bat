99d7ee68af3506c799cef37a180c4d9b759688e5
AmoAmonRa <eilia1322@gmail.com>
2025-12-06 12:53:47 -0800

    icon for the compiled code

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ echo Installing PyInstaller...
 pip install pyinstaller
 
 echo Building executable...
-pyinstaller --onefile --windowed yt_downloader.py
+pyinstaller --onefile --windowed --icon=icon128.png yt_downloader.py
 
 echo Build complete! Check the 'dist' folder for yt_downloader.exe.
